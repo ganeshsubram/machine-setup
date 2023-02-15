6cfcd04887005feacba58b4c198168e7e25e6c3b
Ganesh Subramaniam <ganeshs@glydways.com>
2023-02-14 18:48:41 -0800

    Comment out wireshark, bluetooth and add egpu switcher

--- provision.sh
+++ provision.sh
@@ -2,40 +2,6 @@
 
 sudo apt update -y
 
-# tools
-sudo apt install -y \
-    beep \
-    fzf \
-    gpustat \
-    ifupdown \
-    intel-gpu-tools \
-    libnotify-bin \
-    net-tools \
-    silversearcher-ag \
-    zip unzip
-
-# bluetooth codecs
-sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
-sudo apt install -y \
-    libldac \
-    pulseaudio-modules-bt \
-    libavcodec-extra58 \
-    libfdk-aac1 \
-    bluez \
-    pulseaudio \
-    pavucontrol
-pulseaudio -k
-systemctl --user enable pulseaudio
-systemctl --user start pulseaudio
-
-# wireshark
-# sudo add-apt-repository -y ppa:wireshark-dev/stable
-# sudo apt-get update -y
-# sudo apt-get install -y wireshark
-# sudo dpkg-reconfigure wireshark-common
-# sudo adduser $USER wireshark
-# sudo usermod -a -G wireshark "$USER"
-
 # bash
 rm ~/.bash_aliases
 rm ~/.bashrc
@@ -43,8 +9,19 @@ ln -s ~/machine-setup/.bash_aliases ~/.bash_aliases
 ln -s ~/machine-setup/.bashrc ~/.bashrc
 source ~/.bashrc
 
+# tools
+sudo apt install -y \
+    fzf \
+    silversearcher-ag \
+    zip unzip \
+    vim\
+    net-tools \
+    ifupdown \
+    gpustat \
+    intel-gpu-tools 
+
 # git
-git config --global user.email "ganeshsubram1@gmail.com"
+git config --global user.email "ganeshs@glydways.com"
 git config --global user.name "Ganesh Subramaniam"
 cp ~/machine-setup/.gitignore.txt ~/.gitignore
 
@@ -60,4 +37,35 @@ tmux source-file ~/.tmux.conf
 rm -rf ~/.tmux/plugins/tmux/scripts
 ln -s ~/machine-setup/tmux/dracula/scripts ~/.tmux/plugins/tmux/
 
+# bluetooth codecs
+# sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
+# sudo apt install -y \
+#     libldac \
+#     pulseaudio-modules-bt \
+#     libavcodec-extra58 \
+#     libfdk-aac1 \
+#     bluez \
+#     pulseaudio \
+#     pavucontrol
+# pulseaudio -k
+# systemctl --user enable pulseaudio
+# systemctl --user start pulseaudio
+
+# wireshark
+# sudo add-apt-repository -y ppa:wireshark-dev/stable
+# sudo apt-get update -y
+# sudo apt-get install -y wireshark
+# sudo dpkg-reconfigure wireshark-common
+# sudo adduser $USER wireshark
+# sudo usermod -a -G wireshark "$USER"
+
+# egpu switcher
+# sudo snap install go --classic
+git clone git@github.com:hertg/egpu-switcher.git
+cd egpu-switcher
+make build -s
+sudo make install -s
+sudo egpu-switcher enable
+
+source ~/.bashrc
 sudo apt autoremove -y
