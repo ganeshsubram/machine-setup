6e895b67d43e66016938c1d31dfce7798a4f0107
Ganesh Subramaniam <ganesh@glydways.com>
2024-04-19 18:55:13 -0700

    Eth0 for network_rate and hardcode NVIDIA for GPU

--- tmux/dracula/scripts/network_rate.sh
+++ tmux/dracula/scripts/network_rate.sh
@@ -5,7 +5,7 @@ export LC_ALL=en_US.UTF-8
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
-interface="wlan0"
+interface="eth0"
 # Set the path to the cache file in the /tmp directory
 cache_file="/tmp/network_stats_cache"
 
